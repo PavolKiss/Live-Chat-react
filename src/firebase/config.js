134d8e09e9e3dcb782611817fa0a66295c9c00a9
PavolKiss <pavol.kiss@akademiasovy.sk>
2019-11-07 10:59:30 +0100

    change config for firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: process.env.FIREBASE_APIKEY || "",
+  authDomain: process.env.FIREBASE_DOMAIN || "",
+  databaseURL: process.env.FIREBASE_DATABASE || "",
+  projectId: process.env.FIREBASE_PROJECTID || "",
+  storageBucket: process.env.FIREBASE_STORAGE || "",
+  messagingSenderId: process.env.FIREBASE_SENDERID || "",
+  appId: process.env.FIREBASE_APPID || ""
 };
