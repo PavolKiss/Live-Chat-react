576be9f3beeeca59fa2f28510c02a03495396636
PavolKiss <pavol.kiss@akademiasovy.sk>
2019-11-08 14:02:11 +0100

    now user can see his name, email...

--- src/containers/AccountPage/accountpage.js
+++ src/containers/AccountPage/accountpage.js
@@ -1,5 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import * as firebase from "../../firebase/index";
+import { AccountWrapper, Username, Email, UserAvatar, Admin } from "./styles";
 
 export const AccountPage = () => {
-  return <div></div>;
+  const [userCredentials, setUserCredentials] = useState({
+    username: "",
+    email: "",
+    avatar: "",
+    isAdmin: false
+  });
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth.onAuthStateChanged(async user => {
+      const isAdmin = user
+        ? (await user.getIdTokenResult()).claims.admin
+        : false;
+
+      if (user) {
+        if (isAdmin) {
+          setUserCredentials({
+            username: user.displayName,
+            email: user.email,
+            avatar: user.photoURL,
+            isAdmin: true
+          });
+        } else {
+          setUserCredentials({
+            username: user.displayName,
+            email: user.email,
+            avatar: user.photoURL,
+            isAdmin: false
+          });
+        }
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const { username, email, avatar, isAdmin } = userCredentials;
+
+  return (
+    <AccountWrapper>
+      <h1>Account Details</h1>
+      <Email>
+        <b>Logged as</b> {email}
+      </Email>
+      <Admin>{isAdmin ? "You are Admin" : ""}</Admin>
+      <Username>
+        <b>Your name is</b> {username}
+      </Username>
+      <UserAvatar src={avatar} />
+    </AccountWrapper>
+  );
 };
---
+++ src/containers/AccountPage/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const AccountWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  font-size: 2rem;
+`;
+
+export const Username = styled.div``;
+
+export const Admin = styled.div``;
+
+export const Email = styled.div``;
+
+export const UserAvatar = styled.img`
+  width: 12.5rem;
+  height: 12.5rem;
+`;
